1dec406f11e97f2342b145348da8d3097505387e
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-04-10 18:16:48 -0400

    oopsie3

--- board/jungle/stm32h7/board.h
+++ board/jungle/stm32h7/board.h
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-#include "boards/board_declarations.h"
-
-#include "stm32h7/lladc.h"
-#include "boards/board_v2.h"
-
-void detect_board_type(void) {
-=======
 #pragma once
 #include "jungle/boards/board.h"
 #include "jungle/stm32h7/lladc.h"
@@ -16,7 +8,6 @@ extern struct board *current_board;
 extern struct board board_v2;
 
 static inline void detect_board_type(void) {
->>>>>>> 98e26738 (adopt source/header model)
   hw_type = HW_TYPE_V2;
   current_board = &board_v2;
 }
