1a60ae4d732b86587a5f2b802b3ebd8a861aad59
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-04-11 13:51:50 -0400

    misc headers and such

--- board/drivers/watchdog.h
+++ board/drivers/watchdog.h
@@ -1,3 +1,5 @@
+#pragma once
+
 typedef enum {
   WATCHDOG_50_MS = (400U - 1U),
   WATCHDOG_500_MS = 4000U,
--- include/board/can_declarations.h
+++ include/board/can_declarations.h
@@ -12,6 +12,8 @@ extern const unsigned char dlc_to_len[16];
   #define CANPACKET_DATA_SIZE_MAX 8U
 #endif
 
+#ifndef CAN_PACKET_FORWARD_H
+#define CAN_PACKET_FORWARD_H
 typedef struct {
   unsigned char fd : 1;
   unsigned char bus : 3;
@@ -23,6 +25,7 @@ typedef struct {
   unsigned char checksum;
   unsigned char data[CANPACKET_DATA_SIZE_MAX];
 } __attribute__((packed, aligned(4))) CANPacket_t;
+#endif
 
 #define GET_BUS(msg) ((msg)->bus)
 #define GET_LEN(msg) (dlc_to_len[(msg)->data_len_code])
